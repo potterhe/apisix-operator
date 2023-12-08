c942ae01dbe440d1ae1d0c03ca9a812b4d31d32f
potterhe <potter.he@gmail.com>
2023-12-08 15:08:45 +0800

    make build work

--- cmd/main.go
+++ cmd/main.go
@@ -72,9 +72,9 @@ func main() {
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
+		Scheme: scheme,
+		//MetricsBindAddress:     metricsAddr,
+		//Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "ac308ef3.apisix.apache.org",
